5a95e9adfb9a09e24ece3303d252e22d1c48f41f
mschuldt <mbschuldt@gmail.com>
2014-05-02 14:26:13 -0700

    better

--- make.sh
+++ make.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd ditaa-images
-rm *
-cd ..
+rm ditaa-images/*
+
 emacs -q --eval "(progn (load-file \"conversion-code.el\")(find-file \"notes.org\")(cs61b-export-to-html)(find-file \"index.org\")(org-export-as-html 3)(kill-emacs))"
 
 #clean
